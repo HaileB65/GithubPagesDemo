4afa85e5db55ad2453b4d7608b1d6210b9ab3ec1
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-07-20 13:12:34 -0500

    Removed external file demo folder.

--- externalfiledemo/script.js
+++
@@ -1,6 +0,0 @@
-function changeColor () {
-  document.getElementById("change-colour").style.color = "blue";
-  document.getElementById("change-colour").innerHTML = "blue";
-};
-
-document.getElementById("button").setAttribute("onClick", "changeColor()")
